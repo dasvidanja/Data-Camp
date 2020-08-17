3055164b4fb339a7696b329789121716ae3f9427
dasvidanja <m-artin@hotmail.it>
2020-08-16 23:11:35 -0400

    Add files via upload

---
+++ Merging DataFrames with Pandas/Merge -DF.py
@@ -0,0 +1,28 @@
+# Merge revenue with managers on 'city': merge_by_city
+merge_by_city = pd.merge(revenue, managers, on='city')
+
+# Print merge_by_city
+print(merge_by_city)
+
+# Merge revenue with managers on 'branch_id': merge_by_id
+merge_by_id = pd.merge(revenue, managers, on = 'branch_id')
+
+# Print merge_by_id
+print(merge_by_id)
+
+# Merge revenue & managers on 'city' & 'branch': combined
+combined = pd.merge(revenue, managers, left_on='city', right_on='branch')
+
+# Print combined
+print(combined)
+# Add 'state' column to revenue: revenue['state']
+revenue['state'] = ['TX','CO','IL','CA']
+
+# Add 'state' column to managers: managers['state']
+managers['state'] = ['TX','CO','CA','MO']
+
+# Merge revenue & managers on 'branch_id', 'city', & 'state': combined
+combined = pd.merge(revenue, managers, on = ['branch_id', 'city', 'state'])
+
+# Print combined
+print(combined)
