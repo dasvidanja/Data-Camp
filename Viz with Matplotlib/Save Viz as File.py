27b592d9777ebe821de40f5b388bc3e16acfa4ea
dasvidanja <m-artin@hotmail.it>
2020-08-29 22:17:32 -0400

    Add files via upload

---
+++ Viz with Matplotlib/Save Viz as File.py
@@ -0,0 +1,9 @@
+# Save as a PNG file
+fig.savefig('my_figure.png')
+
+# Save as a PNG file with 300 dpi
+fig.savefig('my_figure_300dpi.png', dpi=300)
+
+# Set figure dimensions and save as a PNG
+fig.set_size_inches([3,5])
+fig.savefig('figure_3_5.png')
